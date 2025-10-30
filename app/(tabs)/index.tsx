a44bd204035b0b93ab9e345049c7b32ccea286d2
Mister D. Ace <ode808prod@gmail.com>
2025-10-30 22:30:19 +0100

    checkpoint 1

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,28 +8,11 @@ import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 export default function Index() {
   const { toggleDarkMode } = useTheme();
 
-  const todos = useQuery(api.todos.getTodos);
-
-  const addTodo = useMutation(api.todos.addTodo);
-
-  const clearTodos = useMutation(api.todos.clearAllTodos);
-
   return (
     <View style={styles.container}>
       <Text style={styles.one}>Hello, Deo</Text>
       <TouchableOpacity style={styles.two} onPress={toggleDarkMode}>
         <Text>Toggle Mode</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity
-        style={styles.two}
-        onPress={() => addTodo({ text: "Learn React Native" })}
-      >
-        <Text>Add A Todo</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity style={styles.two} onPress={() => clearTodos()}>
-        <Text>Clear Todos</Text>
       </TouchableOpacity>
     </View>
   );
